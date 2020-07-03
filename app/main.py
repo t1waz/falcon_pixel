90f841e431c8b8d7f057a52acb58da1337257cdd
Michal Milewicz <t1waz>
2020-07-03 11:00:14 +0200

    adding load balancer

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import falcon
+import constants
 
 
 app = falcon.API()
@@ -10,7 +11,17 @@ class HelloResource:
         response.body = 'hello'
 
 
+class VerifyResource:
+    def on_get(self, request, response):
+        response.status = falcon.HTTP_200
+        response.body = constants.VERIFY_TOKEN
+
+
 hello_resource = HelloResource()
+verify_resource = VerifyResource()
+
 
 app.add_route('/hello', hello_resource)
+app.add_route(f'/{constants.VERIFY_TOKEN}.txt', verify_resource)
+
 
